7a647d38a472ad233df916385a8d5ebab88d606f
Anamika625 <96674509+Anamika625@users.noreply.github.com>
2024-03-19 22:26:26 +0530

    COde Added

--- Employee-Review-System-main/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-console.log('router reloaded');
-
-router.use('/', require('./users'));
-router.use('/review', require('./reviews'));
-
-module.exports = router;
